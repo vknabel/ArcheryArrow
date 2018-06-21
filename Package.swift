783938dd7c6110fee08293898c2fa9405dfd2e2a
Valentin Knabel <dev@vknabel.com>
2018-06-21 22:47:16 +0200

    Bumped dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vknabel/ArrowKit.git", .branch("master")),
         .package(url: "https://github.com/vknabel/Archery.git", .branch("master")),
-        .package(url: "https://github.com/JohnSundell/Unbox.git", from: "2.0.0"),
+        .package(url: "https://github.com/JohnSundell/Unbox.git", from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
